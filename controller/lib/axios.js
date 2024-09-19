b657cb4001ed4538d13fd8f9348641b1bf469761
kno-be <beknoblauch18@gmail.com>
2024-09-19 12:10:22 +0200

    upd: working again

--- controller/lib/axios.js
+++ controller/lib/axios.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const axios = require("axios");
 const MYTOKEN = process.env.MYTOKEN;
 
 const BASE_URL = `https://api.telegram.org/bot${MYTOKEN}`;
-
 function getAxiosInstance() {
     return {
         get(method, params) {
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const { handler } = require('./controller');
 const port = process.env.PORT || 3000;
-require('dotenv').config();
 
 const app = express();
 app.use(express.json());
